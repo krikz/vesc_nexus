6b3970cd60e640a504329fbc9aa30059e1a149bb
kek <keeeek@kukorek.kek>
2025-09-02 03:59:23 +0700

    fixes

--- launch/vesc_nexus_node.launch.py
+++ launch/vesc_nexus_node.launch.py
@@ -1,20 +1,25 @@
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    config = LaunchConfiguration('config', default=os.path.join(
-        get_package_share_directory('vesc_nexus'),
+    # Путь к папке config в пакете vesc_nexus
+    config_pkg_share = FindPackageShare('vesc_nexus')
+
+    # Аргумент 'config' с дефолтом — single_can_quad.yaml
+    config_arg = DeclareLaunchArgument(
         'config',
-        'vesc_nexus_example.yaml'
-    ))
+        default_value=PathJoinSubstitution([config_pkg_share, 'config', 'single_can_quad.yaml']),
+        description='Path to config file to load'
+    )
+
+    # Получаем значение аргумента
+    config = LaunchConfiguration('config')
 
     return LaunchDescription([
-        DeclareLaunchArgument('config', description='Path to config file'),
-
+        config_arg,
         Node(
             package='vesc_nexus',
             executable='vesc_nexus_node',
