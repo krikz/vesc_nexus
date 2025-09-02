563bc7f8fae74bc8863970c396a0b152ada66b7d
kokoko@koko.ko <kokoko@koko.ko>
2025-09-02 19:36:24 +0700

    restructure

--- src/driver/launch/vesc_driver_node.launch.py
+++ src/driver/launch/vesc_driver_node.launch.py
@@ -1,22 +1,27 @@
 import os
-
 from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.actions import LogInfo
 
 
 def generate_launch_description():
-    conf = [PathJoinSubstitution([
-                FindPackageShare('vesc_driver'), 'config', 'single_can_quad.yaml'])
-            ]
-    print(conf)
+    # Создаем подстановку для пути к YAML-файлу
+    config_path = PathJoinSubstitution([
+        FindPackageShare('vesc_driver'), 'config', 'single_can_quad.yaml'
+    ])
+
+    # Выводим путь к конфигурации в лог
     return LaunchDescription([
+        # Логируем путь к файлу
+        LogInfo(msg=["Configuration file path: ", config_path]),
+
+        # Запускаем ноду с параметрами
         Node(
             package='vesc_driver',
             executable='vesc_driver_node',
             name='vesc_driver_node',
-            parameters=conf,
+            parameters=[config_path],
         ),
-
     ])
