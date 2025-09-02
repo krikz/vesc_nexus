99ecf3c76fbf780ea3c739326dcb4315f18f1df5
kokoko@koko.ko <kokoko@koko.ko>
2025-09-02 16:01:21 +0700

    restructure

--- src/driver/launch/vesc_driver_node.launch.py
+++ src/driver/launch/vesc_driver_node.launch.py
@@ -1,31 +1,30 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-    # Путь к папке config в пакете vesc_driver
-    config_pkg_share = FindPackageShare('vesc_driver')
 
-    # Аргумент 'config' с дефолтом — single_can_quad.yaml
-    config_arg = DeclareLaunchArgument(
+    vesc_config = os.path.join(
+        get_package_share_directory('vesc_driver'),
         'config',
-        default_value=PathJoinSubstitution([config_pkg_share, 'config', 'single_can_quad.yaml']),
-        description='Path to config file to load'
-    )
-
-    # Получаем значение аргумента
-    config = LaunchConfiguration('config')
-
+        'single_can_quad.yaml'
+        )
     return LaunchDescription([
-        config_arg,
+        DeclareLaunchArgument(
+            name="config",
+            default_value=vesc_config,
+            description="VESC yaml configuration file.",
+            ),
         Node(
             package='vesc_driver',
             executable='vesc_driver_node',
             name='vesc_driver_node',
-            parameters=[config],
-            output='screen',
-            emulate_tty=True,
+            parameters=[LaunchConfiguration("config")]
         ),
+
     ])
