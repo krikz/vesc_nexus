5c1696ff94d001c3dac240514141e80788745649
DESKTOP-DJ10EF5\krikz <denis.grishko@pra-lab.ru>
2025-09-02 15:21:34 +0700

    restructure

--- src/driver/launch/vesc_driver_node.launch.py
+++ src/driver/launch/vesc_driver_node.launch.py
@@ -5,8 +5,8 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # Путь к папке config в пакете vesc_nexus
-    config_pkg_share = FindPackageShare('vesc_nexus')
+    # Путь к папке config в пакете vesc_driver
+    config_pkg_share = FindPackageShare('vesc_driver')
 
     # Аргумент 'config' с дефолтом — single_can_quad.yaml
     config_arg = DeclareLaunchArgument(
@@ -21,9 +21,9 @@ def generate_launch_description():
     return LaunchDescription([
         config_arg,
         Node(
-            package='vesc_nexus',
-            executable='vesc_nexus_node',
-            name='vesc_nexus_node',
+            package='vesc_driver',
+            executable='vesc_driver_node',
+            name='vesc_driver_node',
             parameters=[config],
             output='screen',
             emulate_tty=True,
