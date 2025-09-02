988adc6fd3c2bcfc486ee323ac43978f26cb2ec8
kokoko@koko.ko <kokoko@koko.ko>
2025-09-02 17:38:40 +0700

    restructure

--- src/driver/launch/vesc_driver_node.launch.py
+++ src/driver/launch/vesc_driver_node.launch.py
@@ -1,30 +1,20 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-
-    vesc_config = os.path.join(
-        get_package_share_directory('vesc_driver'),
-        'config',
-        'single_can_quad.yaml'
-        )
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name="config",
-            default_value=vesc_config,
-            description="VESC yaml configuration file.",
-            ),
         Node(
             package='vesc_driver',
             executable='vesc_driver_node',
             name='vesc_driver_node',
-            parameters=[LaunchConfiguration("config")]
+            parameters=[PathJoinSubstitution([
+                FindPackageShare('vesc_driver'), 'config', 'single_can_quad.yaml'])
+            ],
         ),
 
     ])
