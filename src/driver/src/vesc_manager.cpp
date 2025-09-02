291cd99bd66bc6cbac8594157e83248f816c3bb6
kokoko@koko.ko <kokoko@koko.ko>
2025-09-02 20:27:47 +0700

    restructure

--- src/driver/src/vesc_manager.cpp
+++ src/driver/src/vesc_manager.cpp
@@ -1,32 +1,33 @@
-#include "driver/vesc_manager.hpp"
+#include "vesc_manager.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp" // Включение типа String
 
-VescManager::VescManager(rclcpp::Node *node) : node_(node) {}
+class VescManager {
+public:
+    explicit VescManager(rclcpp::Node* node) : node_(node) {}
 
-void VescManager::addVesc(const std::string& interface_name, uint8_t id, const std::string& label) {
-    // Создаём топики с использованием логического имени
-    std::string topic_prefix = "vesc/" + label;
-    std::string state_topic = topic_prefix + "/state";
-    std::string command_topic = topic_prefix + "/command";
+    void addVesc(const std::string& interface_name, uint8_t id, const std::string& label) {
+        std::string topic_prefix = "vesc/" + label;
+        std::string state_topic = topic_prefix + "/state";
+        std::string command_topic = topic_prefix + "/command";
 
-    RCLCPP_INFO(rclcpp::get_logger("VescManager"), "Adding VESC: Interface=%s, ID=%d, Label=%s",
-                interface_name.c_str(), id, label.c_str());
+        RCLCPP_INFO(rclcpp::get_logger("VescManager"), "Adding VESC: Interface=%s, ID=%d, Label=%s",
+                    interface_name.c_str(), id, label.c_str());
 
-    // Пример создания публикаторов/подписчиков
-    auto state_publisher = this->create_publisher<std_msgs::msg::String>(state_topic, 10);
-    auto command_subscriber = this->create_subscription<std_msgs::msg::String>(
-        command_topic, 10, [this](const std_msgs::msg::String::SharedPtr msg) {
-            RCLCPP_INFO(rclcpp::get_logger("VescManager"), "Received command: %s", msg->data.c_str());
-        });
+        // Создание публикатора
+        auto state_publisher = node_->create_publisher<std_msgs::msg::String>(state_topic, 10);
 
-    // Сохраняем интерфейс и ID
-    interfaces_[interface_name].emplace_back(id, label, state_publisher, command_subscriber);
-}
+        // Создание подписчика
+        auto command_subscriber = node_->create_subscription<std_msgs::msg::String>(
+            command_topic, 10, [this](const std_msgs::msg::String::SharedPtr msg) {
+                RCLCPP_INFO(rclcpp::get_logger("VescManager"), "Received command: %s", msg->data.c_str());
+            });
 
-void VescManager::start()
-{
-    for (auto &pair : can_interfaces_) {
-        pair.second->startListening([this](const can_frame &frame) {
-            // TODO: ??????? ? ???????? ????????? VESC
-        });
+        // Сохранение интерфейса и ID
+        interfaces_[interface_name].emplace_back(id, label, state_publisher, command_subscriber);
     }
-}
+
+private:
+    rclcpp::Node* node_; // Указатель на ноду
+    std::map<std::string, std::vector<std::tuple<uint8_t, std::string, rclcpp::Publisher<std_msgs::msg::String>::SharedPtr, rclcpp::Subscription<std_msgs::msg::String>::SharedPtr>>> interfaces_;
+};
