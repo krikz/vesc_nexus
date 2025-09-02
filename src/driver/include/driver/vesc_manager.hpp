e8840e8214fe4004ab869162d26f8bf11886cd23
DESKTOP-DJ10EF5\krikz <denis.grishko@pra-lab.ru>
2025-09-02 20:33:41 +0700

    restructure

--- src/driver/include/driver/vesc_manager.hpp
+++ src/driver/include/driver/vesc_manager.hpp
@@ -1,22 +1,36 @@
 #ifndef VESC_MANAGER_HPP_
 #define VESC_MANAGER_HPP_
 
-#include "vesc_device.hpp"
-#include "can_interface.hpp"
 #include <map>
 #include <memory>
+#include <string>
+#include <vector>
+#include <tuple>
 
-class VescManager
-{
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class VescManager {
 public:
-  VescManager(rclcpp::Node *node);
-  void addVesc(const std::string& interface_name, uint8_t id, const std::string& label);
-  void start();
+    // Конструктор: принимает указатель на ноду
+    explicit VescManager(rclcpp::Node* node);
+
+    // Метод для добавления VESC
+    void addVesc(const std::string& interface_name, uint8_t id, const std::string& label);
 
 private:
-  rclcpp::Node *node_;
-  std::map<std::pair<std::string, uint8_t>, std::unique_ptr<VescDevice>> vesc_devices_;
-  std::map<std::string, std::unique_ptr<CanInterface>> can_interfaces_;
+    rclcpp::Node* node_; // Указатель на ноду
+
+    // Структура для хранения информации о VESC
+    struct VescInfo {
+        uint8_t id;
+        std::string label;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr state_publisher;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr command_subscriber;
+    };
+
+    // Карта для хранения интерфейсов и их VESC
+    std::map<std::string, std::vector<VescInfo>> interfaces_;
 };
 
 #endif // VESC_MANAGER_HPP_
--- src/driver/src/vesc_manager.cpp
+++ src/driver/src/vesc_manager.cpp
@@ -1,34 +1,30 @@
-#include "driver/vesc_manager.hpp"
 #include "vesc_manager.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp" // Включение типа String
+#include "std_msgs/msg/string.hpp"
 
-class VescManager {
-public:
-    explicit VescManager(rclcpp::Node* node) : node_(node) {}
+// Конструктор
+VescManager::VescManager(rclcpp::Node* node) : node_(node) {}
 
-    void addVesc(const std::string& interface_name, uint8_t id, const std::string& label) {
-        std::string topic_prefix = "vesc/" + label;
-        std::string state_topic = topic_prefix + "/state";
-        std::string command_topic = topic_prefix + "/command";
+// Метод для добавления VESC
+void VescManager::addVesc(const std::string& interface_name, uint8_t id, const std::string& label) {
+    // Формируем префикс для топиков
+    std::string topic_prefix = "vesc/" + label;
+    std::string state_topic = topic_prefix + "/state";
+    std::string command_topic = topic_prefix + "/command";
 
-        RCLCPP_INFO(rclcpp::get_logger("VescManager"), "Adding VESC: Interface=%s, ID=%d, Label=%s",
-                    interface_name.c_str(), id, label.c_str());
+    // Логирование
+    RCLCPP_INFO(node_->get_logger(), "Adding VESC: Interface=%s, ID=%d, Label=%s",
+                interface_name.c_str(), id, label.c_str());
 
-        // Создание публикатора
-        auto state_publisher = node_->create_publisher<std_msgs::msg::String>(state_topic, 10);
+    // Создание публикатора
+    auto state_publisher = node_->create_publisher<std_msgs::msg::String>(state_topic, 10);
 
-        // Создание подписчика
-        auto command_subscriber = node_->create_subscription<std_msgs::msg::String>(
-            command_topic, 10, [this](const std_msgs::msg::String::SharedPtr msg) {
-                RCLCPP_INFO(rclcpp::get_logger("VescManager"), "Received command: %s", msg->data.c_str());
-            });
+    // Создание подписчика
+    auto command_subscriber = node_->create_subscription<std_msgs::msg::String>(
+        command_topic, 10, [this](const std_msgs::msg::String::SharedPtr msg) {
+            RCLCPP_INFO(node_->get_logger(), "Received command: %s", msg->data.c_str());
+        });
 
-        // Сохранение интерфейса и ID
-        interfaces_[interface_name].emplace_back(id, label, state_publisher, command_subscriber);
-    }
-
-private:
-    rclcpp::Node* node_; // Указатель на ноду
-    std::map<std::string, std::vector<std::tuple<uint8_t, std::string, rclcpp::Publisher<std_msgs::msg::String>::SharedPtr, rclcpp::Subscription<std_msgs::msg::String>::SharedPtr>>> interfaces_;
-};
+    // Сохраняем информацию о VESC
+    interfaces_[interface_name].push_back(VescInfo{id, label, state_publisher, command_subscriber});
+}
