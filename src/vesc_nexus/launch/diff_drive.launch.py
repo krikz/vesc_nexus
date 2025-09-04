b712ff8285af7eef1fd94951ac188dbb2d7a80d7
DESKTOP-DJ10EF5\krikz <denis.grishko@pra-lab.ru>
2025-09-05 04:07:52 +0700

    Revert "odometry"
    This reverts commit 801f318533075273a27e83256ac0aea06e5a1b37.

--- src/vesc_nexus/launch/diff_drive.launch.py
+++ src/vesc_nexus/launch/diff_drive.launch.py
@@ -1,46 +1,26 @@
-# launch/diff_drive.launch.py
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
-from launch.substitutions import Command, FindPackageShare
+from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    # Путь к URDF через xacro
-    pkg_share = get_package_share_directory('vesc_nexus')
-    urdf_path = os.path.join(pkg_share, 'urdf', 'robot.urdf.xacro')
-    
-    # Конфиг контроллера
-    controller_config = os.path.join(
-        get_package_share_directory('vesc_nexus'),
-        'config',
-        'robot_controller.yaml'
-    )
+    urdf_path = os.path.join(get_package_share_directory('vesc_nexus'), 'urdf', 'robot.urdf.xacro')
+    controller_config = os.path.join(get_package_share_directory('vesc_nexus'), 'config', 'robot_controller.yaml')
 
     return LaunchDescription([
-        # Публикуем раскрытый URDF
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{
-                'robot_description': ParameterValue(
-                    Command(['xacro', urdf_path]), value_type=str
-                )
-            }]
-        ),
-
-        # Запускаем spawner с явным указанием типа
+            parameters=[controller_config,{'robot_description': urdf_path}],
+            arguments=[urdf_path]),
         Node(
             package='controller_manager',
             executable='spawner',
-            arguments=[
-                'diff_drive_controller',
-                '--controller-manager', '/controller_manager',
-                '--controller-type', 'diff_drive_controller/DiffDriveController'
-            ],
-            output='screen'
+            parameters=[controller_config],
+            arguments=["diff_drive_controller", "-c", "/controller_manager", "-t", "diff_drive_controller/DiffDriveController"],
         ),
     ])
