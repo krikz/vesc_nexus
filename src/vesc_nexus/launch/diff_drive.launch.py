4700bf469a7b1e6dd45addfda17a0cd356302211
DESKTOP-DJ10EF5\krikz <denis.grishko@pra-lab.ru>
2025-09-05 03:19:34 +0700

    Revert "odometry"
    This reverts commit c7331aca3c9660d2be23e648b400eb17a31126ae.

--- src/vesc_nexus/launch/diff_drive.launch.py
+++ src/vesc_nexus/launch/diff_drive.launch.py
@@ -2,13 +2,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
-from launch.substitutions import Command
+from launch.substitutions import Command, FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     # Путь к URDF
-    urdf_path = os.path.join(get_package_share_directory('vesc_nexus'), 'urdf', 'robot.urdf.xacro')
+    urdf_path = FindPackageShare('vesc_nexus').find('vesc_nexus') + '/urdf/robot.urdf.xacro'
 
     # Путь к конфигу
     controller_config = os.path.join(
