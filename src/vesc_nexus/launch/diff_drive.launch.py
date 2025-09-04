4d3b431345b6614d97e4b7de9991ef0c0a9e0c3d
kokoko@koko.ko <kokoko@koko.ko>
2025-09-05 04:13:49 +0700

    odometry

--- src/vesc_nexus/launch/diff_drive.launch.py
+++ src/vesc_nexus/launch/diff_drive.launch.py
@@ -5,8 +5,8 @@ from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
 import os
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import FileContent, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare, FileContent
 
 def generate_launch_description():
     urdf = FileContent(PathJoinSubstitution([FindPackageShare('vesc_nexus', 'robot.urdf.xacro')]))
