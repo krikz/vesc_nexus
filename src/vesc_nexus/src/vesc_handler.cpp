3f0d08b4b9d32676136dc98d5c99d5c82a0d7d2d
kokoko@koko.ko <kokoko@koko.ko>
2025-09-05 02:37:15 +0700

    odometry

--- src/vesc_nexus/src/vesc_handler.cpp
+++ src/vesc_nexus/src/vesc_handler.cpp
@@ -9,6 +9,9 @@ VescHandler::VescHandler(uint8_t can_id, const std::string& label,
     : can_id_(can_id), label_(label), limits_(limits), wheel_radius_(wheel_radius),
      pole_pairs_(poles / 2), min_erpm_(min_erpm)
 {
+  RCLCPP_INFO(rclcpp::get_logger("VescHandler"), "Initialized VescHandler: "
+        "label='%s', can_id=%d, wheel_radius=%.3fm, poles=%d (%d pole pairs), min_erpm=%ld",
+        label_.c_str(), can_id_, wheel_radius_, poles, pole_pairs_, min_erpm_);
     last_state_.label = label;
     last_state_.alive = false;
 }
