f50cb43d908b392bb9c018a36003d118b3799c53
kokoko@koko.ko <kokoko@koko.ko>
2025-09-05 04:26:13 +0700

    odometry

--- src/vesc_nexus/launch/diff_drive.launch.py
+++ src/vesc_nexus/launch/diff_drive.launch.py
@@ -1,23 +1,19 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FileContent, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    urdf = FileContent(PathJoinSubstitution([FindPackageShare('vesc_nexus', 'robot.urdf.xacro')]))
+    urdf_path = os.path.join(get_package_share_directory('vesc_nexus'), 'urdf', 'robot.urdf.xacro')
     controller_config = os.path.join(get_package_share_directory('vesc_nexus'), 'config', 'robot_controller.yaml')
 
     return LaunchDescription([
         Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[controller_config,{'robot_description': urdf}],
-            arguments=[urdf]),
+            package='controller_manager',
+            executable='ros2_control_node',
+            parameters=[controller_config, {'robot_description': Command(['xacro ', urdf_path])}]
+        ),
         Node(
             package='controller_manager',
             executable='spawner',
