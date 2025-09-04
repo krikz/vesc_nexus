812092e67b962fc533f2cb0353cc1130e486b4d5
kokoko@koko.ko <kokoko@koko.ko>
2025-09-05 03:14:48 +0700

    odometry

--- src/vesc_nexus/launch/diff_drive.launch.py
+++ src/vesc_nexus/launch/diff_drive.launch.py
@@ -22,7 +22,9 @@ def generate_launch_description():
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{'robot_description': Command(['xacro', urdf_path])}],
+            parameters=[{'robot_description': ParameterValue(
+                    Command(['xacro', urdf_path]), value_type=str
+                )}],
             output='screen'
         ),
 
